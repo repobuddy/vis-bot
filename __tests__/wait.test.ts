64fd14fe28c42c4faaec0799d814dbe2e1dfaec6
unional <homawong@gmail.com>
2026-01-03 19:15:33 -0800

    pnpm, biome

--- __tests__/wait.test.ts
+++ __tests__/wait.test.ts
@@ -1,24 +1,25 @@
 /**
  * Unit tests for src/wait.ts
  */
+import { describe, it, expect } from 'vitest'
 import { wait } from '../src/wait.js'
 
 describe('wait.ts', () => {
-  it('Throws an invalid number', async () => {
-    const input = parseInt('foo', 10)
+	it('Throws an invalid number', async () => {
+		const input = Number.parseInt('foo', 10)
 
-    expect(isNaN(input)).toBe(true)
+		expect(Number.isNaN(input)).toBe(true)
 
-    await expect(wait(input)).rejects.toThrow('milliseconds is not a number')
-  })
+		await expect(wait(input)).rejects.toThrow('milliseconds is not a number')
+	})
 
-  it('Waits with a valid number', async () => {
-    const start = new Date()
-    await wait(500)
-    const end = new Date()
+	it('Waits with a valid number', async () => {
+		const start = new Date()
+		await wait(500)
+		const end = new Date()
 
-    const delta = Math.abs(end.getTime() - start.getTime())
+		const delta = Math.abs(end.getTime() - start.getTime())
 
-    expect(delta).toBeGreaterThan(450)
-  })
+		expect(delta).toBeGreaterThan(450)
+	})
 })
